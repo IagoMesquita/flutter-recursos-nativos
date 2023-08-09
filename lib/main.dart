d700222c21ccfb44e3a56a5eac1b10506837b98a
Iago Mesquita <iagomesquitaalves@gmail.com>
2023-08-09 08:51:39 -0300

    feat: ChangeNotifierProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:recusos_nativos/provider/great_places.dart';
 import 'package:recusos_nativos/screens/place_form_screen.dart';
 import 'package:recusos_nativos/screens/places_list_sceen.dart';
 import 'package:recusos_nativos/utils/app_routes.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,23 +15,26 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final ThemeData theme = ThemeData();
-    return MaterialApp(
-      title: 'Recursos Nativos',
-      debugShowCheckedModeBanner: false,
-      theme: theme.copyWith(
-        colorScheme: theme.colorScheme.copyWith(
-          primary: Colors.indigo,
-          secondary: Colors.amber
-        )
+    return ChangeNotifierProvider(
+      create: (ctx) => GreatPlaces(),
+      child: MaterialApp(
+        title: 'Recursos Nativos',
+        debugShowCheckedModeBanner: false,
+        theme: theme.copyWith(
+          colorScheme: theme.colorScheme.copyWith(
+            primary: Colors.indigo,
+            secondary: Colors.amber
+          )
+        ),
+        // theme: ThemeData(
+        //    primarySwatch: Colors.indigo,
+        //    accentColor: Colors.amber
+        // ),
+        home: const PlacesListScreen(),
+              routes: {
+          AppRoutes.PLACE_FORM: (context) =>  const PlaceFormScreen(),
+        },
       ),
-      // theme: ThemeData(
-      //    primarySwatch: Colors.indigo,
-      //    accentColor: Colors.amber
-      // ),
-      home: const PlacesListScreen(),
-            routes: {
-        AppRoutes.PLACE_FORM: (context) =>  const PlaceFormScreen(),
-      },
     );
   }
 }
