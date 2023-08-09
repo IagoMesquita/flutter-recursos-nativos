a2b11bbd8d806728d96d7e34bdcc55b7462ac581
Iago Mesquita <iagomesquitaalves@gmail.com>
2023-08-09 08:51:11 -0300

    feat: creat path with name image

--- lib/widgets/image_input.dart
+++ lib/widgets/image_input.dart
@@ -2,6 +2,8 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:path/path.dart' as path;
+import 'package:path_provider/path_provider.dart' as syspaths;
 
 class ImageInput extends StatefulWidget {
   final Function onSelectImage; 
@@ -28,7 +30,15 @@ class _ImageInputState extends State<ImageInput> {
       _storedImage = File(imageFile.path);
     });
 
-    widget.onSelectImage(_storedImage);
+    final appDir = await syspaths.getApplicationDocumentsDirectory();
+    String fileName = path.basename(_storedImage!.path);
+    final savedImage = await _storedImage!.copy('${appDir.path}/$fileName');
+
+    print('APPDIR: $appDir');
+    print('FINENAME: $fileName');
+    print('SAVEDIMAGE: $savedImage');
+
+    widget.onSelectImage(savedImage);
   }
 
   @override
