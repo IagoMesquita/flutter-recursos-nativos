1a9c4203b4861f99810b93570e348b20f789553e
Iago Mesquita <iagomesquitaalves@gmail.com>
2023-08-12 12:54:44 -0300

    feat> ddl database

---
+++ lib/utils/db_util.dart
@@ -0,0 +1,21 @@
+import 'package:sqflite/sqflite.dart' as sql;
+import 'package:path/path.dart' as path;
+
+class DbUtil {
+  // Por ser um banco relacional, antes precisamos saber se o banco e a tabela já foram criados antes de criar os dados.
+  // Por isso vamos criar um comando ddl(data definition language)
+
+  static Future<sql.Database> database() async {
+    final dbPath = await sql.getDatabasesPath();
+
+    return sql.openDatabase(
+      path.join(dbPath, 'places.db'),
+      onCreate: (db, version) {
+        return db.execute(
+          'CREATE TABLE places (id TEXT PRIMARY KEY, title TEXT, image TEXT)'
+        );
+      },
+      version: 1,
+    );
+  }
+}
