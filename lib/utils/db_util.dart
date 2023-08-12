05fab70c71ab4da9e779ea4b37c589a049ff7af1
Iago Mesquita <iagomesquitaalves@gmail.com>
2023-08-12 13:01:57 -0300

    feature: insert util

--- lib/utils/db_util.dart
+++ lib/utils/db_util.dart
@@ -12,10 +12,18 @@ class DbUtil {
       path.join(dbPath, 'places.db'),
       onCreate: (db, version) {
         return db.execute(
-          'CREATE TABLE places (id TEXT PRIMARY KEY, title TEXT, image TEXT)'
-        );
+            'CREATE TABLE places (id TEXT PRIMARY KEY, title TEXT, image TEXT)');
       },
       version: 1,
+    );
+  }
+
+  static Future<void> insert(String table, Map<String, Object> data) async {
+    final db = await DbUtil.database();
+    await db.insert(
+      table,
+      data,
+      conflictAlgorithm: sql.ConflictAlgorithm.replace,
     );
   }
 }
