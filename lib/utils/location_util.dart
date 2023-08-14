0321327552f37adfcc9f277cc391b9ea6ede62cd
Iago Mesquita <iagomesquitaalves@gmail.com>
2023-08-14 17:45:55 -0300

    feat: location util

---
+++ lib/utils/location_util.dart
@@ -0,0 +1,10 @@
+import 'dart:convert';
+
+const GOOGLE_API_KEY = '';
+
+class LocationUtil {
+  static String generateLocationPreviewImage(
+      {double latitude = 0.0, double longitude = 0.0}) {
+    return 'https://maps.googleapis.com/maps/api/staticmap?center=$latitude,$longitude&zoom=13&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C$latitude,$longitude&key=$GOOGLE_API_KEY';
+  }
+}
